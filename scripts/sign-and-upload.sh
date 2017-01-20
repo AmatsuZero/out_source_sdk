260aceeabf82f3626ef3e0509a688cfdd5829984
姜振华 <jiangzhenhua@123juzi.com>
2017-01-20 18:14:24 +0800

    打包并上传Fir/蒲公英

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -12,3 +12,14 @@ PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/$PROFILE_
 OUTPUTDIR="$PWD/build/Release-iphoneos"
 
 xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APPNAME.app" -o "$OUTPUTDIR/$APPNAME.ipa" -sign "$DEVELOPER_NAME" -embed "$PROVISIONING_PROFILE"
+
+#fir上传ipa并分发
+fir p $OUTPUTDIR/$APPNAME.ipa -T $FIR_APP_TOEKN
+
+curl -F "file=@$OUTPUTDIR/$APPNAME.ipa" \
+-F "uKey=$PGYER_UKEY" \
+-F "_api_key=$PGYER_APIKEY" \
+http://www.pgyer.com/apiv1/app/upload
+
+RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
+RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
