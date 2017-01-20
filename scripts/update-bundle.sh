9dfe8559a15cdb9c71e3de137c526ea32b869486
姜振华 <jiangzhenhua@123juzi.com>
2017-01-20 16:40:25 +0800

    no message

--- scripts/update-bundle.sh
+++ scripts/update-bundle.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-xctool -workspace IntegratedSDK.xcworkspace -scheme Demo -sdk iphoneos -configuration Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO 'CODE_SIGN_RESOURCE_RULES_PATH=$(SDKROOT)/ResourceRules.plist'
+
+if [ ! -z "$INFO_PLIST" ]; then
+  /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $TRAVIS_BUILD_NUMBER" "$INFO_PLIST"
+  echo "Set CFBundleVersion to $TRAVIS_BUILD_NUMBER"
+fi
+
+if [ ! -z "$BUNDLE_IDENTIFIER" ]; then
+  /usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier $BUNDLE_IDENTIFIER" "$INFO_PLIST"
+  echo "Set CFBundleIdentifier to $BUNDLE_IDENTIFIER"
+fi
+
+if [ ! -z "$BUNDLE_DISPLAY_NAME" ]; then
+  /usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName $BUNDLE_DISPLAY_NAME" "$INFO_PLIST"
+  echo "Set CFBundleDisplayName to $BUNDLE_DISPLAY_NAME"
+fi
